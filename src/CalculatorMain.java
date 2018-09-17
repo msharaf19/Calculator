e8eae413a3f1139b7f955f823172b99a1b447de0
msharaf19 <42978512+msharaf19@users.noreply.github.com>
2018-09-17 14:28:59 -0400

    Add files via upload

---
+++ src/CalculatorMain.java
@@ -0,0 +1,14 @@
+
+public class CalculatorMain {
+
+	public static void main(String[] args) 
+	{
+		Calculator calc = new Calculator();
+		calc.add(2, 4);
+		
+		System.out.print(calc.add(2, 4));
+		
+
+	}
+
+}
